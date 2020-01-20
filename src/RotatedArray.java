0549caaf84da3a8c9b6c78a195696b1c4c139f60
kobeaibiancheng <wyx160413@yeah.net>
2020-01-20 22:35:30 +0800

    Rotated Array

--- src/RotatedArray.java
+++ src/RotatedArray.java
@@ -1,6 +1,23 @@
 import java.util.Arrays;
 
 public class RotatedArray {
+    /**
+     * 双重循环
+     * 时间复杂度：O(kn)
+     * 空间复杂度：O(1)
+     * @param nums
+     * @param k
+     */
+    public static void rotateArray(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int tmp = nums[nums.length-1];
+            for (int j = nums.length-1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = tmp;
+        }
+    }
     private static void reverse(int[] nums,int start,int end) {
         while(start < end) {
             int tmp = nums[start];
@@ -10,6 +27,14 @@ public class RotatedArray {
             end--;
         }
     }
+
+    /**
+     * 翻转
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param nums
+     * @param k
+     */
     public static void rotate(int[] nums, int k) {
         k = k%nums.length;
         reverse(nums,0,nums.length-1);
