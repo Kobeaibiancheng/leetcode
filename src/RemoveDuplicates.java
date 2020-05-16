77a30467c1430d891bf8da5f1728419bd8451b68
kobeaibiancheng <wyx160413@yeah.net>
2020-05-16 22:41:26 +0800

    删除排序数组中的重复项

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,12 @@
+public class RemoveDuplicates {
+    /**
+     * 删除排序后的数组
+     * 如果没有排序，可以先排序下
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        int j = 0;
+    }
+}
