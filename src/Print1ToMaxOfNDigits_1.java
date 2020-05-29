144c38db0243effdb170c19a30cf005a5e6f4f7a
kobeaibiancheng <wyx160413@yeah.net>
2020-05-29 22:16:47 +0800

    打印1到最大的n位数

--- src/Print1ToMaxOfNDigits_1.java
+++ src/Print1ToMaxOfNDigits_1.java
@@ -1,9 +1,28 @@
 public class Print1ToMaxOfNDigits_1 {
+
+    public static void print1ToMaxOfNDigits_1(int n) {
+        char[] arr = new char[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = '0';
+        }
+        while (!increment(arr)) {
+            printNumber(arr);
+        }
+    }
+    private static boolean increment(char[] arr) {
+
+    }
+
+    private static void printNumber(char[] arr) {
+    }
+
+
+
     /**
      * 这样的代码 当 n 很大的时候，不管是int还是long long都会溢出
      * @param n
      */
-    public static void print1ToMaxOfNDigits_1(int n) {
+    /*public static void print1ToMaxOfNDigits_1(int n) {
         int max = 1;
         for (int i = 0; i < n; i++) {
             max *= 10;
@@ -11,9 +30,9 @@ public class Print1ToMaxOfNDigits_1 {
         for (int i = 1; i < max; i++) {
             System.out.print(i + " ");
         }
-    }
+    }*/
 
     public static void main(String[] args) {
-        print1ToMaxOfNDigits_1(1    );
+        print1ToMaxOfNDigits_1(1);
     }
 }
