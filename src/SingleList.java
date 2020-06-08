2cf76a6487aa1ece2ec02b593645341a141cfccf
kobeaibiancheng <wyx160413@yeah.net>
2020-06-08 22:37:53 +0800

    合并两个单链表

--- src/SingleList.java
+++ src/SingleList.java
@@ -68,9 +68,31 @@ public class SingleList {
         }
         cur.next = headA != null ? headA : headB;
         return newHead.next;
-
     }
 
+    /**
+     * 原地合并两个排序链表  不new节点
+     * 递归
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+        
+    }
     public static void main(String[] args) {
         ListNode a = new ListNode(1);
         ListNode b = new ListNode(2);
