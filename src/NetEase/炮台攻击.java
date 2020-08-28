f9e78d6ce326f55ed9e798dd8ed4533cbf7aba6e
kobeaibiancheng <wyx160413@yeah.net>
2020-08-28 21:33:52 +0800

    网易 炮台攻击

---
+++ src/NetEase/炮台攻击.java
@@ -0,0 +1,19 @@
+package NetEase;
+
+import java.util.Scanner;
+
+public class 炮台攻击 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int[] xy = new int[8];
+        int harm = 0;
+        int j = 6;
+        for(int i = 0; i < xy.length; i++){
+            xy[i] = sc.nextInt();
+        }
+        for(int i = 0; i < xy.length; i++){
+
+        }
+    }
+}
