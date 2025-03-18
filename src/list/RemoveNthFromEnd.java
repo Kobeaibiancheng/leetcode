2c3cd86672f51274e367ff7ed4b2d559fcfc5a22
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-03-18 22:03:51 +0800

    Update RemoveNthFromEnd.java

--- src/list/RemoveNthFromEnd.java
+++ src/list/RemoveNthFromEnd.java
@@ -1,1 +1,28 @@
+package list;
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode p1 = pre;
+        ListNode p2 = pre;
+        while(n != 0){
+            p1 = p1.next;
+            n--;
+        }
+
+        while(p1.next != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p2.next = p2.next.next;
+        return pre.next;
+    }
+}
