c0b28da97cfbab5503dccd7628eff7a2efad9df2
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-08-05 23:26:44 +0800

    leetcode 无重复字符最长字串

--- src/string/LengthOfLongestSubstring.java
+++ src/string/LengthOfLongestSubstring.java
@@ -1,5 +1,24 @@
 class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s){
+        int left = 0;
+        int maxLen = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),i+1);
+            }else{
+                left = Math.max(map.get(s.charAt(i)),left);
+                map.remove(s.charAt(i));
+                map.put(s.charAt(i),i+1);
+            }
+            maxLen = Math.max(i-left+1,maxLen);
+        }
+        return maxLen;
+        
+    }
+    /**
+    public int lengthOfLongestSubstring(String s) {
+        
         int max = 0;
         for(int i = 0; i < s.length(); i++){
             Set<Character> set = new HashSet<>();
@@ -18,5 +37,6 @@ class LengthOfLongestSubstring {
             }
         }
         return max;
-    }    
+    }
+     */
 }
