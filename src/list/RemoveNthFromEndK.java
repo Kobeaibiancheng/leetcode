478a7c43a4ce45bbc2476b30f2f05e99c839743c
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-08-08 10:18:44 +0800

    leetcode 删除链表倒数第k个节点

---
+++ src/list/RemoveNthFromEndK.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveNthFromEndK {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode cur = head;
+        ListNode pre = head;
+        int count = 0;
+        while(cur != null){
+            cur = cur.next;
+            count++;
+        }
+        if(n == count){
+            return head.next;
+        }
+        int i = 0;
+        cur = head;
+        while(cur != null && i < n){
+            cur = cur.next;
+            i++;
+        }
+        if(cur == null){
+            return cur;
+        }
+        while(cur.next != null){
+            cur = cur.next;
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+        return head;
+        
+    }
+}
