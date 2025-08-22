85dc93ddb85c917b0d0823f1e812e7e73908c539
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-08-22 16:04:07 +0800

    volatile关键字

---
+++ src/thread/TestVolatile.java
@@ -0,0 +1,29 @@
+package thread;
+
+
+public class TestVolatile {
+    private static volatile boolean flag=true;
+    public static void main(String[] args){
+        new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                    if(flag){
+                        System.out.println("线程A");
+                        flag=false;
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                    if(!flag){
+                        System.out.println("线程B");
+                        flag=true;
+                    }
+                }
+            }
+        }).start();
+    }
+}
